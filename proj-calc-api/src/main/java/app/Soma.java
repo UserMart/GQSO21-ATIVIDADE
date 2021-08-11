fd4682692001c322bf98acbc228137fb4cbba8d3
jose_martins924 <secretariovirtual65@gmail.com>
2021-08-11 20:26:29 +0000

    função soma

--- proj-calc-api/src/main/java/app/Soma.java
+++ proj-calc-api/src/main/java/app/Soma.java
@@ -2,11 +2,25 @@ package app;
 
 import io.jooby.Jooby;
 import io.jooby.annotations.*;
+import io.jooby.exception.*;
+import io.jooby.BadRequestException.*;
 
-@Path("/Soma/{op}")
+@Path("/Soma/{x}/{y}")
 public class Soma {
     @GET
-    public String RotaSoma () {
-        return "Soma";
+    public String RotaSoma (@PathVariable("x"),
+     String x, @PathVariable("y"), String y) {
+         
+        //conversão de double e verificacao de erros
+        try{
+            double result=Double.parseDouble(x) + Double.parseDouble(y);
+              // retorno dos valores
+               return String.format("%2f", result);
+        } 
+        catch (NumberFormatException nfe){
+            throw  new BadRequestException(String.format("Este Parâmetro é inválido:\"%s\"", result);
+
+        }	
+     
     }
 }
