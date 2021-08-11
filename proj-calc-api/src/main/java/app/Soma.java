4f4c4e7dd66a526e2286ba609cb47a7fd5b14a07
jose_martins924 <secretariovirtual65@gmail.com>
2021-08-11 20:28:49 +0000

    soma

--- proj-calc-api/src/main/java/app/Soma.java
+++ proj-calc-api/src/main/java/app/Soma.java
@@ -18,7 +18,7 @@ public class Soma {
                return String.format("%2f", result);
         } 
         catch (NumberFormatException nfe){
-            throw  new BadRequestException(String.format("Este Parâmetro é inválido:\"%s\"", result);
+            throw  new BadRequestException(String.format("Este Parâmetro é inválido!", result);
 
         }	
      
