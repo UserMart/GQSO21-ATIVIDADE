0bf1694c4f3f46eb78c4f41c461283f649535d96
jose_martins924 <secretariovirtual65@gmail.com>
2021-09-01 14:09:50 +0000

    ajeitando a fução soma

--- calc-api/src/main/java/app/Soma.java
+++ calc-api/src/main/java/app/Soma.java
@@ -5,20 +5,19 @@ import io.jooby.annotations.*;
 import io.jooby.exception.*;
 import io.jooby.BadRequestException.*;
 
-@Path("/Soma/{x}/{y}")
 public class Soma {
+    @Path("/Soma/{x}/{y}")
     @GET
-    public String RotaSoma (@PathVariable("x"),
-     String x, @PathVariable("y"), String y) {
+    public String RotaSoma (@PathParam String x, @PathParam String y) {
          
         //conversão de double e verificacao de erros
         try{
-            double result=Double.parseDouble(x) + Double.parseDouble(y);
+            double result = (Double.parseDouble(x)) + (Double.parseDouble(y));
               // retorno dos valores
                return String.format("%2f", result);
-        } 
-        catch (NumberFormatException nfe){
-            throw  new BadRequestException(String.format("Este Parâmetro é inválido!", result));
+
+        } catch (NumberFormatException nfe){
+            throw  new BadRequestException(String.format("Este Parâmetro é inválido!\"%s\"", result));
 
         }	
      
