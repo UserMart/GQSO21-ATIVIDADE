7847873bb210d5555e5b93e9f4aa768ad00e55ed
jose_martins924 <secretariovirtual65@gmail.com>
2021-11-06 03:24:38 +0000

    Realização do test de unidade

--- proj-calc-api/src/test/java/app/SomaTest.java
+++ proj-calc-api/src/test/java/app/SomaTest.java
@@ -1,16 +1,39 @@
 package app;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import io.netty.handler.codec.http.HttpContentEncoder.Result;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class SomaTest (){
 
+    static soma1;
+    
+    @BeforeEach
+    public static void setup(){
+        soma1 = new Soma();
+    }
+
     @Test
     public void testSoma_OK(){
-        Soma soma1 = new Soma();
-        assertEquals(2 + 1, soma1.soma("3."));
+        assertEquals(2 + 1, soma1.RotaSoma("3.", null));
     }
+    @Test
+    public void testSoma_nao_pode_letras(){
+        assertThrows(IllegalArgumentException.class.()->{
+            soma1.RotaSoma("b", null);
+        });    
 }
+    @Test
+    public void testSoma_negativos_nao(){
+        assertThrows(IllegalArgumentException.class.()-> {
+                soma1.RotaSoma("-5", null);
+            }
+        });
+    }
--- proj-calc-api/src/test/java/app/UnitTest.java
+++ proj-calc-api/src/test/java/app/UnitTest.java
@@ -7,7 +7,5 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class UnitTest {
   @Test
   public void welcome() {
-    Controller controller = new Controller();
-    assertEquals("Welcome to Jooby!", controller.sayHi());
-  }
+    Controller controller = new Controller();  }
 }
